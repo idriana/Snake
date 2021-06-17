3e96890fffc1932866bbb8d8df940ffdaec58a2a
idriana <57726132+idriana@users.noreply.github.com>
2021-06-17 18:21:44 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mad_f/CLionProjects/cppprj
-# Build directory: C:/Users/mad_f/CLionProjects/cppprj/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doctest")
--- cmake-build-debug/doctest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mad_f/CLionProjects/cppprj/doctest
-# Build directory: C:/Users/mad_f/CLionProjects/cppprj/cmake-build-debug/doctest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
